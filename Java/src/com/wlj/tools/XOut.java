5ecd9c55d2a2e109441f0adcc530582de4a05ac0
Mr.Wang <wang_lijun_it@163.com>
2016-09-12 11:18:55 +0800

    默认打开debug开关

--- Java/src/com/wlj/tools/XOut.java
+++ Java/src/com/wlj/tools/XOut.java
@@ -1,7 +1,7 @@
 package com.wlj.tools;
 
 public class XOut {
-	private static final boolean debug = false;
+	private static final boolean debug = true;
 	public static void out(String msg){
 		if(debug){
 			System.out.println(msg);
